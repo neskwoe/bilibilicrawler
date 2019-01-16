834ed963de681d8e53667d0f1300ece131286f6b
nesk_office laptop <nesk_woe@hotmail.com>
2019-01-16 16:40:00 +0800

    v0.1

--- lib/main.py
+++ lib/main.py
@@ -11,23 +11,21 @@ db = MySQL('bibiliviewdata') #连接mysql server，使用bibiliviewdata db
 
 crawler = bilibilicrawler()
 
-for day in range(1, 60):   #循环获得60天的data
+for i in range(1, 39): #根据rid获取不同分类的data
 
-    for i in range(1, 39): #根据rid获取不同分类的data
+    resultlist = crawler.retrieve_rank_data('https://api.bilibili.com/x/web-interface/ranking/region?rid={}'.format(i)) #使用api获得ranking data
 
-        resultlist = crawler.retrieve_rank_data('https://api.bilibili.com/x/web-interface/ranking/region?rid={}'.format(i)) #使用api获得ranking data
+    for result in resultlist:
 
-        for result in resultlist:
+        try:
 
-            try:
+            db.sql_insert('rank_data', result)
 
-                db.sql_insert('rank_data', result)
+        except:
 
-            except:
+            print('data issue, please verify the result data: ' + result)
 
-                print('data issue, please verify the result data: ' + result)
+print(str(datetime.datetime.now()) + 'data 获取完毕')
 
-    print(str(datetime.datetime.now()) + 'data 获取完毕')
 
-    time.sleep(86400) #停留一天
 
