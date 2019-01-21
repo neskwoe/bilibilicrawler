2ae677649cdffd9ae3f8780c5349f8c171d57dc0
neskwoe <43666012+neskwoe@users.noreply.github.com>
2019-01-21 08:35:20 +0800

    v0.01

--- lib/main.py
+++ lib/main.py
@@ -15,32 +15,13 @@ crawler = bilibilicrawler()
 
 if ('getrank' == func):   #使用get rank data function
 
-    try:
+    crawler.get_rank_data(db)
 
-        repeat_days = sys.argv[2]
-
-        for i in repeat_days():
-
-            crawler.get_rank_data(db)
-
-            time.sleep(86400)
-
-    except:
-
-        print('default to 60 days')
-
-        for i in range(1, 60):
-            
-            crawler.get_rank_data(db)
-
-            time.sleep(86400)
-
-elif('getpic' == func):
+elif('getpic' == func):    #使用getpic function
 
     crawler.get_homepage_pic()
 
-
-elif('getoverall' == func):
+elif('getoverall' == func):  #使用getoverall function
 
     crawler.requestViewData()
 
